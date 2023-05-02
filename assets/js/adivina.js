577d61af28d5fb5cbc1e66dd9e5de36845970203
Siria <sirifranco00@gmail.com>
2023-05-02 01:51:51 -0700

    instrucciones en archivo de adivina.js

--- assets/js/adivina.js
+++ assets/js/adivina.js
@@ -10,43 +10,42 @@ function jugar(){
 }
 
 function menorNumero(){
-    const minNumero = 1;
-    var mensaje0;
-    var obtenerVerNumero = document.getElementById("verNumero").innerHTML;
-    var obtenerNumMaximo = document.getElementById("numMaximo").innerHTML;
-    const parseNumMaximo = parseInt(obtenerNumMaximo);
+    const minNumero = 1; //mínima
+    var mensaje0; // variable para uso más adelante
+    var obtenerVerNumero = document.getElementById("verNumero").innerHTML; //Obtener número generado de manera aleatoria por función "jugar"
+    var obtenerNumMaximo = document.getElementById("numMaximo").innerHTML; //obtener el número máximo indicado
+    const parseNumMaximo = parseInt(obtenerNumMaximo); //parse a número máximo
     if (obtenerVerNumero < parseNumMaximo) {
         (document.getElementById("numMaximo").innerHTML = obtenerVerNumero);
-    }
+    } // se aplica if para que el número máximo se actualice por el nuevo
     applyRandom0;
-    var applyRandom0 = Math.floor(Math.random() * obtenerVerNumero - minNumero + 2);
-    mensaje0 = (document.getElementById("verNumero").innerHTML = applyRandom0);
-
+    var applyRandom0 = Math.floor(Math.random() * obtenerVerNumero - minNumero + 2); // aplicado aleatorio para obtener nuevo número 
+    mensaje0 = (document.getElementById("verNumero").innerHTML = applyRandom0); //número aleatorio colocado en posición original de función "jugar"
 }
 
 function mayorNumero(){
     var mensaje0;
-    const obtenerVerNumero = document.getElementById("verNumero").innerHTML;
-    const obtenerNumMinimo = document.getElementById("numMinimo").innerHTML;
-    const parseNumMinimo = parseInt(obtenerVerNumero);
-    const maxNumero = 100;
-    console.log(obtenerVerNumero);
-    console.log(parseNumMinimo);
+    const obtenerVerNumero = document.getElementById("verNumero").innerHTML; //Obtener numero random original indicado por función "jugar"
+    const obtenerNumMinimo = document.getElementById("numMinimo").innerHTML; // Obtener número mínimo indicado en párrafo
+    const parseNumMinimo = parseInt(obtenerVerNumero); // Parse a número mínimo
+    var obtenerNumMaximo = document.getElementById("numMaximo").innerHTML; //Obtener número máximo indicado en párrafo 
+    const parseNumMaximo = parseInt(obtenerNumMaximo); //Parse a número máximo
+    const maxNumero = parseNumMaximo; // colocar como constante el valor máximo obtenido
+    console.log(obtenerVerNumero); 
     console.log(obtenerNumMinimo);
+    console.log(parseNumMinimo);
+    console.log(obtenerNumMaximo);
+    console.log(parseNumMaximo);
+    console.log(maxNumero);
     if (obtenerVerNumero > obtenerNumMinimo) {
         (document.getElementById("numMinimo").innerHTML = obtenerVerNumero);
-    }
+    } // aplicar un if para cambiar el valor mínimo, afirmando que el número pensado es mayor al actual mínimo
     console.log(obtenerVerNumero);
-    applyRandom;
-    var applyRandom = Math.floor(Math.random() * (maxNumero - parseNumMinimo +1) + parseNumMinimo);
+    applyRandom; //variable random para renovar el número random original
+    var applyRandom = Math.floor(Math.random() * (maxNumero - parseNumMinimo +1) + parseNumMinimo); //se usa la variable actual de número máximo y el mínimo actual para crear un nuevo número random entre esos valores
     console.log(applyRandom);
-    mensaje0 = (document.getElementById("verNumero").innerHTML = applyRandom);
+    mensaje0 = (document.getElementById("verNumero").innerHTML = applyRandom); //cambiar el número principal (el de botón jugar) por el nuevo número random
     console.log(mensaje0);
-
-    /* mensaje0 = (document.getElementById("verNumero").innerHTML = applyRandom);
-    var minima;
-    minima = (document.getElementById("verNumero").innerHTML = applyRandom);
-    mensaje0 = (document.getElementById("numMinimo").innerHTML = minima); */
 }
 
 function correctoNumero(){
